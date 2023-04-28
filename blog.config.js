e1c6f62b43410d4fb8670a2c476e75209665a95b
betterjerry <51983520+betterjerry@users.noreply.github.com>
2023-04-28 23:42:43 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '杰D杂文馆', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '我是一个普通的写作爱好者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.jerryhao.cn', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'jdnotion,博客,杰D杂文馆',杰D, // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'jdnotion,博客,杰D,杰D杂文馆', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'm@jerryhao.cn', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
